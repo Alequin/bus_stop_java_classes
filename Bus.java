6fe20feb76c06f9ee128db8c0b409d21ef1e35cc
James Cox <j-cox03@hotmail.com>
2017-06-26 16:04:06 +0100

    Corrected passengers array type to Person in class Bus

--- Bus.java
+++ Bus.java
@@ -1,9 +1,23 @@
 public class Bus{
 
-  private int[] passengers;
+  private Person[] passengers;
 
   public Bus(int size){
-    this.passengers = new int[size];
+    this.passengers = new Person[size];
   }
 
+  public int getPassengerCount(){
+    int count = 0;
+    for(int j=1; j<passengers.length; j++){
+      if(passengers[j] == null){
+        count = j;
+        break;
+      }
+    }
+    return count;
+  }
+
+  public void add(Person person){
+
+  }
 }
