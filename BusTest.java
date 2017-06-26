4d600e00c4b41e836a7fca8b4db3a8a606404e13
James Cox <j-cox03@hotmail.com>
2017-06-26 15:54:41 +0100

    Created test files for Bus and Person and made @before methods

---
+++ BusTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BusTest{
+
+  @before
+  public void setup(){
+    Bus bus = new Bus(10);
+  }
+
+}
---
+++ PersonTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PersonTest{
+
+  @before
+  public void setup(){
+    Person person = new Person("Andrew");
+  }
+
+}
